a27461b248baee69fc31ba3855fd1e3d87894ede
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2021-04-15 17:36:21 +0900

    マイグレーションの外部キーの重複を解消

--- db/migrate/20210409075437_create_prototypes.rb
+++ db/migrate/20210409075437_create_prototypes.rb
@@ -6,7 +6,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.text   :concept,             null: false
       # タイトル、キャッチコピー、コンセプトを保存するための記述。
       t.references :user, foreign_key: true
-      t.references :comment, foreign_key: true
+      # t.references :comment, foreign_key: true
       # ユーザーのデータを参照したいので、レファレンスを使い、外部キーを記述した。
       t.timestamps
     end
